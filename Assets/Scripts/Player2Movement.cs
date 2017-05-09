91358cb68960b695ca2152ba6ed4a9ee4856b7c7
Flo <florian.kaindl@stud.uni-regensburg.de>
2017-05-09 18:44:27 +0200

    Resitricted Movement of Players
    Works for Player 1, but not for Player 2?

--- Assets/Scripts/Player2Movement.cs
+++ Assets/Scripts/Player2Movement.cs
@@ -3,28 +3,85 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player2Movement : MonoBehaviour {
-    public float moveSpeed;
 
-    // Use this for initialization
-    void Start()
-    {
+	public float moveSpeed;
+	public float maxVerticalDistance = 8;
+	public float maxHorizontalDistance = 12;
 
-    }
+	private Rigidbody2D rb;
+	private float currentDistance;
+	private GameObject[] players;
+	private Vector3 horizontalMovement;
+	private Vector3 verticalMovement;
+	private Vector3 newPos;
+	private float axisVertical;
+	private float axisHorizontal;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetAxisRaw("Horizontalp2") > 0.5f || Input.GetAxisRaw("Horizontalp2") < -0.5f)
-        {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontalp2") * moveSpeed * Time.deltaTime, 0f, 0f));
+	// Use this for initialization
+	void Start()
+	{
+		rb = GetComponent <Rigidbody2D>();
+		players = GameObject.FindGameObjectsWithTag ("Player");
+	}
 
-        }
-        if (Input.GetAxisRaw("Verticalp2") > 0.5f || Input.GetAxisRaw("Verticalp2") < -0.5f)
-        {
+	// Update is called once per frame
+	void Update()
+	{
+		horizontalMovement = new Vector3(0,0,0);
+		verticalMovement = new Vector3(0,0,0);
+		axisHorizontal = 0;
+		axisVertical = 0;
+		newPos = new Vector2(0,0);
+		axisHorizontal = Input.GetAxisRaw ("Horizontalp2");
+		axisVertical = Input.GetAxisRaw ("Verticalp2");
 
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Verticalp2") * moveSpeed * Time.deltaTime, 0f));
+		if (axisHorizontal > 0.5f || axisHorizontal < -0.5f)
+		{
+			for (int i = 0; i < players.Length; i++) 
+			{
+				if (players [i] != this) 
+				{
+					currentDistance = (players [i].transform.position.x - transform.position.x);
 
-        }
+					if (Mathf.Abs (currentDistance) < maxHorizontalDistance) 
+					{
+						horizontalMovement = new Vector3 (axisHorizontal * moveSpeed * Time.deltaTime, 0, 0);
+					} 
+					else if ((currentDistance < 0 && axisHorizontal > -0.5f) || (currentDistance > 0 && axisHorizontal < 0.5f) ) 
+					{
+						horizontalMovement = new Vector3(0,0,0);
+					} 
+				}
+			}
 
-    }
+
+		}
+
+		if (axisVertical > 0.5f || axisVertical < -0.5f)
+		{
+			for (int i = 0; i < players.Length; i++) {
+
+				if (players [i] != this) {
+					currentDistance = (players [i].transform.position.y - transform.position.y);
+
+					if (Mathf.Abs (currentDistance) < maxVerticalDistance) 
+					{
+						verticalMovement = new Vector3 (0, axisVertical * moveSpeed * Time.deltaTime, 0);
+					} 
+					else if ((currentDistance < 0 && axisVertical > -0.5f) || (currentDistance > 0 && axisVertical < 0.5f) ) {
+						verticalMovement = new Vector3 (0, 0, 0);
+					} 
+				}
+			}
+		}
+
+		newPos = (Vector2) rb.position + (Vector2) horizontalMovement + (Vector2) verticalMovement;
+		rb.MovePosition (newPos);
+
+	}
+	void FixedUpdate() 
+	{
+
+	}
 }
+
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,27 +5,82 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     public float moveSpeed;
+	public float maxVerticalDistance = 8;
+	public float maxHorizontalDistance = 12;
+
+	private Rigidbody2D rb;
+	private float currentDistance;
+	private GameObject[] players;
+	private Vector3 horizontalMovement;
+	private Vector3 verticalMovement;
+	private Vector3 newPos;
+	private float axisVertical;
+	private float axisHorizontal;
 
     // Use this for initialization
     void Start()
     {
-        
+		rb = GetComponent <Rigidbody2D>();
+		players = GameObject.FindGameObjectsWithTag ("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") > 0.5f || Input.GetAxisRaw("Horizontal") < -0.5f)
+		horizontalMovement = new Vector3(0,0,0);
+		verticalMovement = new Vector3(0,0,0);
+		axisHorizontal = 0;
+		axisVertical = 0;
+		newPos = new Vector2(0,0);
+		axisHorizontal = Input.GetAxisRaw ("Horizontal");
+		axisVertical = Input.GetAxisRaw ("Vertical");
+
+		if (axisHorizontal > 0.5f || axisHorizontal < -0.5f)
         {
-            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal") * moveSpeed * Time.deltaTime, 0f, 0f));
+			for (int i = 0; i < players.Length; i++) 
+			{
+				if (players [i] != this) 
+				{
+					currentDistance = (players [i].transform.position.x - transform.position.x);
 
+					if (Mathf.Abs (currentDistance) < maxHorizontalDistance) 
+					{
+						horizontalMovement = new Vector3 (axisHorizontal * moveSpeed * Time.deltaTime, 0, 0);
+					} 
+					else if ((currentDistance < 0 && axisHorizontal > -0.5f) || (currentDistance > 0 && axisHorizontal < 0.5f) ) 
+					{
+						horizontalMovement = new Vector3(0,0,0);
+					} 
+				}
+			}
+
+			
         }
-        if (Input.GetAxisRaw("Vertical") > 0.5f || Input.GetAxisRaw("Vertical") < -0.5f)
+
+		if (axisVertical > 0.5f || axisVertical < -0.5f)
         {
+			for (int i = 0; i < players.Length; i++) {
 
-            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical") * moveSpeed * Time.deltaTime, 0f));
+				if (players [i] != this) {
+					currentDistance = (players [i].transform.position.y - transform.position.y);
 
+					if (Mathf.Abs (currentDistance) < maxVerticalDistance) 
+					{
+						verticalMovement = new Vector3 (0, axisVertical * moveSpeed * Time.deltaTime, 0);
+					} 
+					else if ((currentDistance < 0 && axisVertical > -0.5f) || (currentDistance > 0 && axisVertical < 0.5f) ) {
+						verticalMovement = new Vector3 (0, 0, 0);
+					} 
+				}
+			}
         }
-        
+
+		newPos = (Vector2) rb.position + (Vector2) horizontalMovement + (Vector2) verticalMovement;
+		rb.MovePosition (newPos);
+
     }
+	void FixedUpdate() 
+	{
+		
+	}
 }
