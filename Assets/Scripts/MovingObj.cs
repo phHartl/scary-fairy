c1d756c244f661ba5467ce57c8c34f7d4207ec9b
Philipp Hartl <Philipp1.Hartl@stud.uni-regensburg.de>
2017-05-10 19:34:23 +0200

    Added MovingObj class
    Players now inherit from movingObj. MovingObj is still a dummy class,
    but with all necessary functions

---
+++ Assets/Scripts/MovingObj.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MovingObj : MonoBehaviour
+{
+	protected float moveSpeed;
+	public LayerMask collisionLayer;
+	protected Rigidbody2D rb2D;
+	protected BoxCollider2D boxCollider;
+	private int _hitpoints;
+	private int _damage;
+	protected Vector2 newPos;
+
+
+
+	// Use this for initialization
+	protected void Start ()
+	{
+		boxCollider = GetComponent<BoxCollider2D>();
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	protected void Update () {
+		
+	}
+
+	protected virtual Vector2 Move() //Report back to Gamemanager -> Animation
+	{
+		return newPos;
+	}
+
+	protected void OnDie() //Report back to Gamemanager
+	{
+
+	}
+
+	private void OnCollisionEnter2D(Collision2D other) //Report back to Gamemanager, Damage? UI changed etc.
+	{
+		throw new System.NotImplementedException();
+	}
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,35 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : MovingObj {
 
     public float moveSpeed;
 	public float maxVerticalDistance = 8;
 	public float maxHorizontalDistance = 12;
 
-	private Rigidbody2D rb;
 	private float currentDistance;
 	private GameObject[] players;
 	private Vector2 horizontalMovement;
 	private Vector2 verticalMovement;
-	private Vector2 newPos;
 	private float axisVertical;
 	private float axisHorizontal;
 
     // Use this for initialization
-	void Awake()
-	{
-		rb = GetComponent <Rigidbody2D>();
-	}
 
 	void Start()
 	{
+		base.Start();
 		players = GameObject.FindGameObjectsWithTag ("Player2");
 	}
 
     // Update is called once per frame
     void Update()
     {
+	    rb2D.MovePosition(Move());
+    }
+
+	protected override Vector2 Move()
+	{
 		horizontalMovement = new Vector2(0,0);
 		verticalMovement = new Vector2(0,0);
 		newPos = new Vector2(0,0);
@@ -39,53 +39,52 @@ public class PlayerMovement : MonoBehaviour {
 		axisVertical = Input.GetAxisRaw ("Vertical");
 
 		if (axisHorizontal > 0.5f || axisHorizontal < -0.5f)
-        {
-			for (int i = 0; i < players.Length; i++) 
+		{
+			for (int i = 0; i < players.Length; i++)
 			{
-				
+
 				{
 					currentDistance = (players [i].transform.position.x - transform.position.x);
 
 					//movement possible if player below distance threshold or moving towards other player
-					if (Mathf.Abs (currentDistance) < maxHorizontalDistance || (currentDistance > 0 && axisHorizontal > 0) || (currentDistance < 0 && axisHorizontal < 0)) 
-					{ 
+					if (Mathf.Abs (currentDistance) < maxHorizontalDistance || (currentDistance > 0 && axisHorizontal > 0) || (currentDistance < 0 && axisHorizontal < 0))
+					{
 						horizontalMovement = new Vector2 (axisHorizontal * moveSpeed * Time.deltaTime, 0);
-					} 
+					}
 					else
 					{
 						horizontalMovement = new Vector2(0,0);
-					} 
+					}
 				}
 			}
 
-			
-        }
+
+		}
 
 		if (axisVertical > 0.5f || axisVertical < -0.5f)
-        {
+		{
 			for (int i = 0; i < players.Length; i++) {
 
-				if (players [i] != this) 
+				if (players [i] != this)
 				{
 					currentDistance = (players [i].transform.position.y - transform.position.y);
 
 					//movement possible if player below distance threshold or moving towards other player
-					if (Mathf.Abs (currentDistance) < maxVerticalDistance || (currentDistance > 0 && axisVertical > 0) || (currentDistance < 0 && axisVertical < 0)) 
+					if (Mathf.Abs (currentDistance) < maxVerticalDistance || (currentDistance > 0 && axisVertical > 0) || (currentDistance < 0 && axisVertical < 0))
 					{
 						verticalMovement = new Vector2 (0, axisVertical * moveSpeed * Time.deltaTime);
-					} 
-					else 
+					}
+					else
 					{
 						verticalMovement = new Vector2 (0, 0);
-					} 
+					}
 				}
 			}
-        }
+		}
 
-		newPos = rb.position + horizontalMovement + verticalMovement;
-		rb.MovePosition (newPos);
-
-    }
+		newPos = rb2D.position + horizontalMovement + verticalMovement;
+		return newPos;
+	}
 	void FixedUpdate() 
 	{
 		
