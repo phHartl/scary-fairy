946ab8382e9158a9cf99396952de2f6bccd74adf
Philipp Hartl <scaredn00b@users.noreply.github.com>
2017-06-13 20:54:25 +0200

    Added support for A*- Pathfinding + demo graph and calculation
    Source: https://arongranberg.com/astar/docs/index.php
    
    Current Version: 4.0.10 Free

--- Assets/Scripts/CasualEnemy.cs
+++ Assets/Scripts/CasualEnemy.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using Pathfinding;
 using UnityEngine;
 
 public class CasualEnemy : Npc {
 
     UnityEngine.UI.Slider slider;
+    public Vector2 targetPosition = new Vector3(28.6f,-30);
+
     // Use this for initialization
     void Start()
     {
         base.Start();
         slider = GetComponentInChildren<UnityEngine.UI.Slider>();
+        Seeker seeker = GetComponent<Seeker>();
+        seeker.StartPath(transform.position, targetPosition, OnPathComplete);
 
         // hitpoints placeholder
         this._hitpoints = 100;
@@ -20,6 +25,11 @@ public class CasualEnemy : Npc {
 
         //movespeed placeholder
         this.moveSpeed = 0;
+    }
+
+    private void OnPathComplete(Path p)
+    {
+        Debug.Log("We did it" + p.error);
     }
 
     // Update is called once per frame
