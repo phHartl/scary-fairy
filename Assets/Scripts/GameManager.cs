08ebe90c2b21dac5e5c74a827a4c0f593cb52b16
Florian Kaindl <florian.kaindl@stud.uni-regensburg.de>
2017-06-20 16:20:44 +0200

    GameManager uploaded
    Manually uploaded GameManager script

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager instance = null;
+    private int level = 1;
+    private string[] levelPaths;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+        initGame();
+    }
+
+    void initGame()
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
