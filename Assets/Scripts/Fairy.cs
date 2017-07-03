e69c2003125967b21efa4255c1ee42940af57c56
kappa4head <lukas.jack@web.de>
2017-07-03 18:25:27 +0200

    Moved attack from LateUpdate() to Update()

--- Assets/Scripts/Fairy.cs
+++ Assets/Scripts/Fairy.cs
@@ -31,15 +31,15 @@ public class Fairy : Player {
         animator.SetFloat("LastMoveX", target.GetComponent<Player>().lastMove.x);
         animator.SetFloat("LastMoveY", target.GetComponent<Player>().lastMove.y);
         novaAnimator.SetBool("NovaAttack", isAttacking);
+        if (Input.GetKeyDown("q") && !isOnCoolDown)
+        {
+            StartCoroutine(Attack());
+        }
     }
 
 
     void LateUpdate () {
         transform.position = target.transform.position + FAIRY_DISTANCE;
-        if (Input.GetKeyDown("q") && !isOnCoolDown)
-        {
-            StartCoroutine(Attack());
-        }
         enchantAttacks();
     }
     //copy pasta MeleePlayer
@@ -55,10 +55,6 @@ public class Fairy : Player {
         isOnCoolDown = false;
      }
 
-    private new void FixedUpdate()
-    {
-
-    }
 
 
     /*
