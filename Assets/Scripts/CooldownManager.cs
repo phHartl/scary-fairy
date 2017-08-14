88035dbe838b72c8eca28cef7f573f96df17c213
Philipp Hartl <scaredn00b@users.noreply.github.com>
2017-08-14 12:54:10 +0200

    Created basic cooldown manager
    Created base of CD manager to work with.

---
+++ Assets/Scripts/CooldownManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CooldownManager : MonoBehaviour {
+
+    public bool[] warriorOnCooldown = new bool[2]; //This needs to be checked by player classes
+    public bool[] rangerOnCooldown = new bool[2];
+    public bool[] fairyOnCooldown = new bool[4];
+    private float[] warriorAbilityCooldowns = { 1f, 5f }; //Attack, defensiveState
+    private float[] rangerAbilityCooldowns = { 1f, 5f }; //Attack, multiShot
+    private float[] fairyAbilityCooldowns = { 2f, 5f, 5f, 8f }; //Attack, fire, ice, speed
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartCooldown(int cooldownIndex, int playerClassIndex) //Start a CD based on which class calls this
+    {
+        {
+            warriorOnCooldown[cooldownIndex] = true;
+            StartCoroutine(WaitForCooldown(warriorAbilityCooldowns[cooldownIndex]));
+            warriorOnCooldown[cooldownIndex] = false;
+            return; //Add event here
+        {
+            rangerOnCooldown[cooldownIndex] = true;
+            StartCoroutine(WaitForCooldown(rangerAbilityCooldowns[cooldownIndex]));
+            rangerOnCooldown[cooldownIndex] = false;
+            return;
+        {
+            fairyOnCooldown[cooldownIndex] = true;
+            StartCoroutine(WaitForCooldown(fairyAbilityCooldowns[cooldownIndex]));
+            return;
+        }
+    }
+
+    private IEnumerator WaitForCooldown(float cooldown)
+    {
+        yield return new WaitForSeconds(cooldown);
+    }
+}
