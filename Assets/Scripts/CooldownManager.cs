5a5f1cfde9de03750fc5a780276b2f3242a04b30
Philipp Hartl <scaredn00b@users.noreply.github.com>
2017-08-14 12:55:10 +0200

    Somehow it didnt commit if statements

--- Assets/Scripts/CooldownManager.cs
+++ Assets/Scripts/CooldownManager.cs
@@ -23,16 +23,19 @@ public class CooldownManager : MonoBehaviour {
 
     public void StartCooldown(int cooldownIndex, int playerClassIndex) //Start a CD based on which class calls this
     {
+        if(playerClassIndex == 0) //Warrior
         {
             warriorOnCooldown[cooldownIndex] = true;
             StartCoroutine(WaitForCooldown(warriorAbilityCooldowns[cooldownIndex]));
             warriorOnCooldown[cooldownIndex] = false;
             return; //Add event here
+        }else if (playerClassIndex == 1) //Ranger
         {
             rangerOnCooldown[cooldownIndex] = true;
             StartCoroutine(WaitForCooldown(rangerAbilityCooldowns[cooldownIndex]));
             rangerOnCooldown[cooldownIndex] = false;
             return;
+        }else //Fairy
         {
             fairyOnCooldown[cooldownIndex] = true;
             StartCoroutine(WaitForCooldown(fairyAbilityCooldowns[cooldownIndex]));
