6a14cbd79d0591dc73cb84d9d8bf5963d06b667f
kappa4head <lukas.jack@web.de>
2017-08-26 18:13:07 +0200

    Menu shows up when hiotting ESC

---
+++ Assets/Scripts/ContinueFromPause.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueFromPause : MonoBehaviour {
+
+    public GameObject menu = GameObject.Find("Container");
+
+	public void resumeGame()
+    {
+        for (int i = 0; i < gameObject.transform.childCount; i++)
+        {
+            gameObject.transform.GetChild(i).gameObject.SetActive(false);
+        }
+        Time.timeScale = 1;
+    }
+
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public string pause;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown(pause))
+        {
+            for(int i = 0; i < gameObject.transform.childCount; i++)
+            {
+                gameObject.transform.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+    }
+}
