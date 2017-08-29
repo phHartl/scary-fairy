e8afa0db25b7bdd5002d4b21be49e7fa55f7ca61
kappa4head <lukas.jack@web.de>
2017-08-29 15:15:15 +0200

    Fixed arrow pitch sound, added ambient music to scenes

--- Assets/Scripts/EnemyProjectile.cs
+++ Assets/Scripts/EnemyProjectile.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class EnemyProjectile : MonoBehaviour
 {
     public Rigidbody2D projectile;
+    private AudioSource projectileSound;
 
     public virtual Rigidbody2D CreateProjectile(Vector3 position, Quaternion rotation, float travelTime)
     {
         Rigidbody2D projectileClone = Instantiate(projectile, position, rotation);
+        if(projectileClone.gameObject.GetComponent<AudioSource>() != null)
+        {
+            projectileSound = projectileClone.gameObject.GetComponent<AudioSource>();
+            projectileSound.pitch = Random.Range(0.75f, 1.25f);
+        }
         Destroy(projectileClone.gameObject, travelTime);
         return projectileClone;
     }
--- Assets/Scripts/PlayerProjectile.cs
+++ Assets/Scripts/PlayerProjectile.cs
@@ -5,14 +5,10 @@ using UnityEngine;
 public class PlayerProjectile : EnemyProjectile
 {
 
-    private AudioSource arrowSound;
-
     // To create an arrow you need its position, rotation and the time it should travel (if we get an general object spawner this class can be a sub class)
     public Rigidbody2D CreateArrow(Vector3 position, Quaternion rotation, float travelTime)
     {
         Rigidbody2D projectileClone = CreateProjectile(position, rotation, travelTime);
-        arrowSound = projectileClone.gameObject.GetComponent<AudioSource>();
-        arrowSound.pitch = Random.Range(0.75f, 1.25f);
         Destroy(projectileClone.gameObject, travelTime);
         return projectileClone;
     }
