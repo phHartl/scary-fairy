5198fe39f69de7de38f5e77175d6e9ac73ea61e9
Philipp Hartl <scaredn00b@users.noreply.github.com>
2017-08-30 16:19:01 +0200

    Added cursor controller

---
+++ Assets/Scripts/ControllerCursorUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ControllerCursorUtility : MonoBehaviour, ISelectHandler
+{
+
+    public RectTransform ControllerCursorTransform;
+    private Vector3 ButtonPosition;
+
+    private void Start()
+    {
+        ButtonPosition = GetComponentInParent<RectTransform>().localPosition;
+    }
+
+    public void OnSelect(BaseEventData eventData)
+    {
+        ControllerCursorTransform.localPosition = ButtonPosition;
+        ControllerCursorTransform.localPosition += new Vector3(-294, 0, 0);
+    }
+}
